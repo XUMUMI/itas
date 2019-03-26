0ddf1ff64ab19606e3b4e78aa1a6da6104a17dbd
Teoan <d277077411@gmail.com>
2019-03-26 09:41:28 +0800

    - add 2Ser to Library

---
+++ lib/Android_2Ser/itas_gson/LoginStatus.java
@@ -0,0 +1,24 @@
+package com.itas.itas.itas_gson;
+
+/**
+ * @author Teoan
+ * @title: LoginStatus
+ * @projectName Android_Client
+ * @description: 登录状态类
+ * @date 2019/3/1817:21
+ */
+public class LoginStatus {
+    /**
+     * status : 1
+     */
+
+    private int status=1;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+}
